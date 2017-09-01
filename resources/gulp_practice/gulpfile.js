62bbc17c6eaffa4807bf13d5bebd274c542c406c
ulgoon <ulgoon89@gmail.com>
2017-09-01 16:20:31 +0900

    edit gulpfile.js edit priority and continuous tasks

--- resources/gulp_practice/gulpfile.js
+++ resources/gulp_practice/gulpfile.js
@@ -2,7 +2,15 @@ var gulp = require('gulp');
 
 
 gulp.task("hello", function(){
-    return console.log("hello gulpworld");
+    return console.log("hello");
 });
 
-gulp.task("default", ["hello"]);
+gulp.task("gulp",["hello"] ,function(){
+    return console.log("gulp");
+});
+
+gulp.task("world",["gulp"] ,function(){
+    return console.log("world");
+});
+
+gulp.task("default", ["world"]);
