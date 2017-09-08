87f87ba62337d688388403eb566f8e93ff187170
ulgoon <ulgoon89@gmail.com>
2017-09-08 11:15:29 +0900

    edit linked_list.js: add append method

--- resources/js_data_structure/linked_list.js
+++ resources/js_data_structure/linked_list.js
@@ -6,4 +6,20 @@ function LinkedList(){
 
     var length = 0;
     var head = null;
+
+        this.append = function(element){
+            var node = new Node(element),
+                current;
+
+            if (head === null){
+                head = node;
+            } else {
+                current = head;
+                while(current.next){
+                    current = current.next;
+                }
+                current.next = node;
+            }
+            length++;
+        };
 }
