1475ac6c6a80586c9edf3f5c95587d18ee733b78
ulgoon <ulgoon89@gmail.com>
2017-09-08 14:40:17 +0900

    init tree.js

---
+++ resources/js_data_structure/tree.js
@@ -0,0 +1,63 @@
+function BinarySearchTree(){
+    var Node = function(key){
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    };
+
+    var root = null;
+
+    var insertNode = function(node, newNode){
+        if (newNode.key < node.key){
+            if(node.left === null){
+                node.left = newNode;
+            } else {
+                insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null){
+                node.right = newNode;
+            } else {
+                insertNode(node.right, newNode);
+            }
+        }
+    };
+
+    this.insert = function(key){
+        var newNode = new Node(key);
+
+        if(node === null){
+            root = newNode;
+        } else {
+            insertNode(root, newNode);
+        }
+    };
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
