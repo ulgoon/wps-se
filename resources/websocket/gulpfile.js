33eabddb82e18d0123e26b48cfe230d82d829586
ulgoon <ulgoon89@gmail.com>
2017-11-17 16:26:11 +0900

    gulp js uglify

--- resources/websocket/gulpfile.js
+++ resources/websocket/gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require('gulp');
+var pump = require('pump');
+
+var uglify = require('gulp-uglify');
 
 var publicPath = {
     src: "./public/src/",
     dest: "./public/dist/"
 }
 
+gulp.task("uglify", function(){
+    pump([
+        gulp.src(publicPath.src + "js/*"),
+        uglify(),
+        gulp.dest(publicPath.dest + "js/")
+    ]);
+});
+
+
+gulp.task("default", ["uglify"]);
+
+/*
 gulp.task("hello", function(){
     return console.log("Hello");
 });
@@ -16,5 +31,5 @@ gulp.task("gulp", ["hello"], function(){
 gulp.task("world", ["gulp"], function(){
     return console.log("world");
 });
+*/
 
-gulp.task("default", ["world"]);
