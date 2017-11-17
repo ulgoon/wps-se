3b109ffc8d2c584dca1d2dc4bb6dbb4ed7c37804
ulgoon <ulgoon89@gmail.com>
2017-11-17 12:29:56 +0900

    broadcast complete

--- resources/websocket/server.js
+++ resources/websocket/server.js
@@ -14,7 +14,9 @@ io.on("connection", function(socket){
     console.log("A user connected");
     socket.on("chat message", function(msg){
         console.log("message: ", msg);
+        io.emit("chat message", msg);
     });
+
     socket.on("disconnect", function(){
         console.log("User disconnected");
     });
