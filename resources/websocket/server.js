1ced2ae4f447603734117726424cd06ea6365f8d
ulgoon <ulgoon89@gmail.com>
2017-11-17 12:05:50 +0900

    add emitConnection --> connection important Connection --> connection

--- resources/websocket/server.js
+++ resources/websocket/server.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
-
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 
 app.set("port", process.env.PORT || 8080);
-
 app.get('/', function(req,res){
     //res.send("Chat start");
     res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on("connection", function(socket){
+    console.log("A user connected");
+    socket.on("chat message", function(msg){
+        console.log("message: ", msg);
+    });
+    socket.on("disconnect", function(){
+        console.log("User disconnected");
+    });
 });
 
 http.listen(app.get("port"), function(){
