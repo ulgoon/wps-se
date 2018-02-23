e8aff48eeccbeb0cec0b251ef570cb80657e77a9
ulgoon <ulgoon89@gmail.com>
2018-02-23 17:10:15 +0900

    serve html with ejs(rendering)

--- resources/websocket-chat/server.js
+++ resources/websocket-chat/server.js
@@ -2,11 +2,19 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3030);
+app.set('views', __dirname +'/views');
+
+app.set('view engines', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req,res){
-    res.send("First express works!");
+    res.render('index.html');
+});
+
+app.get('/chat', function(req,res){
+    res.render('chat.html');
 });
 
 app.listen(app.get('port'), function(){
-    console.log("Express server is working on.." + app.get('port'));
+    console.log("Express server is working on.. http://localhost:" + app.get('port'));
 });
