c9ca12a1b5002c2c00f59aeb2b610c826f61d249
ulgoon <ulgoon89@gmail.com>
2018-02-23 16:54:54 +0900

    express for websocket init

---
+++ resources/websocket-chat/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3030);
+
+app.get('/', function(req,res){
+    res.send("First express works!");
+});
+
+app.listen(app.get('port'), function(){
+    console.log("Express server is working on.." + app.get('port'));
+});
