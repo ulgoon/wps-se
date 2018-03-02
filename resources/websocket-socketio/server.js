8791ba7fa0bde6fecf64d1bf2671e7c1e9c0823d
ulgoon <ulgoon89@gmail.com>
2018-03-02 16:40:33 +0900

    add feature: rendering with ejs

--- resources/websocket-socketio/server.js
+++ resources/websocket-socketio/server.js
@@ -2,9 +2,18 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3030);
+app.set('view engines', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+
 
 app.get('/', function(req, res){
-    res.send("First Express works!");
+    //res.send("First Express works!");
+    res.render('index.html');
+});
+
+app.get('/chat', function(req, res){
+    res.render('chat.html');
 });
 
 app.listen(app.get('port'), function(){
