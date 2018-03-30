5fb4bc8d0e749cb068ee27a905ac758713a4aaee
ulgoon <ulgoon89@gmail.com>
2018-03-30 16:22:47 +0900

    concat

--- resources/websocket-socketio/gulpfile.js
+++ resources/websocket-socketio/gulpfile.js
@@ -2,10 +2,11 @@
 var gulp = require('gulp');
 var pump = require('pump');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 
 // gulp informations
-var publickPath = {
+var publicPath = {
     src: './public/src/',
     dest: './public/dist/',
 }
@@ -19,13 +20,21 @@ gulp.task("gulpworld", ["hello"], function(){
     return console.log("Hello gulpworld!")
 });
 
+gulp.task("concat", function(){
+    pump([
+        gulp.src([publicPath.src + "main.js", publicPath.src + "main2.js"]),
+        concat('main.min.js'),
+        gulp.dest(publicPath.src)
+    ]);
+});
+
 gulp.task("uglify", function(){
     pump([
-        gulp.src(publickPath.src + '*.js'),
+        gulp.src(publicPath.src + 'main.min.js'),
         uglify(),
-        gulp.dest(publickPath.dest)
+        gulp.dest(publicPath.dest)
     ]);
 });
 
 // gulp default task
-gulp.task("default", ["uglify"]);
+gulp.task("default", ["concat", "uglify"]);
