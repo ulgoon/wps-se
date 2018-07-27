1cf736005eab79c50723addcad4d01bf844f357d
ulgoon <ulgoon89@gmail.com>
2018-07-27 11:36:27 +0900

    websocket: render with ejs

--- resources/websocket/server.js
+++ resources/websocket/server.js
@@ -1,12 +1,23 @@
 var express = require('express');
 
+
 var app = express();
 
 
 app.set('port', process.env.PORT || 8888);
+app.set('view engines', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.set('views', __dirname + '/views');
+
 
 app.get('/', function(req,res){
-    res.send('Express server works!');
+    //res.send('Express server works!');
+    res.render('index.html');
+});
+
+app.get('/chat', function(req,res){
+    res.render('chat.html');
 });
 
 app.listen(app.get('port'), function(){
