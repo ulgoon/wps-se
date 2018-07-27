5fbed58a13339c8fbdf826862c4bde4c3e9036a0
ulgoon <ulgoon89@gmail.com>
2018-07-27 12:43:17 +0900

    websocket: complete

--- resources/websocket/server.js
+++ resources/websocket/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
-
-
 var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
 
 
 app.set('port', process.env.PORT || 8888);
@@ -20,6 +23,20 @@ app.get('/chat', function(req,res){
     res.render('chat.html');
 });
 
-app.listen(app.get('port'), function(){
+io.on('connection', function(socket){
+    console.log('A user connected');
+
+    socket.on("chat message", function(msg){
+        console.log("message: " + msg);
+        io.emit("chat message", msg);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('User Disconnected');
+    });
+});
+
+
+http.listen(app.get('port'), function(){
     console.log('Express Server is running on localhost:' + app.get('port'));
 });
